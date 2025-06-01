146b2c32958bb68418a0034d94ed125722553a4a
Maxion38 <147092829+Maxion38@users.noreply.github.com>
2025-06-01 22:27:28 +0200

    Update woody.py

--- TP10/services/api/order_service/woody.py
+++ TP10/services/api/order_service/woody.py
@@ -1,3 +1,9 @@
+flask
+flask-cors
+redis
+mysql-connector-python
+pika
+guy@swarm-manager-1:~/tp10/TP10/woodytoys/services/api/order_service$ cat woody.py
 # Voici les différentes fonctions que l'api de Woody pourrait utiliser
 #
 # Il y a beacoup de "sleep", et de ralentissement qui ont pour but de simuler une grosse charge et/ou
@@ -7,12 +13,7 @@
 # ( ce fichier sera, à terme dans une lib externe pour rendre plus clair la séparation)
 
 
-from werkzeug.serving import run_simple
 from mysql.connector import connect, Error
-from time import sleep
-
-LONG_WAIT_TIME = 5  # seconds
-SHORT_WAIT_TIME = 5
 
 
 def my_connect():
@@ -29,66 +30,6 @@ def my_connect():
     return mydb, mycursor
 
 
-def get_last_product():
-    mydb, mycursor = my_connect()
-
-    mycursor.execute("LOCK TABLES product READ;")
-
-    mycursor.execute("SELECT name, sleep(15) FROM product ORDER BY id DESC LIMIT 1;")
-
-    last_product = mycursor.fetchone()
-
-    mycursor.execute("select count(*) from product;")
-    product_count = mycursor.fetchone()
-
-    # sleep(SHORT_WAIT_TIME)
-
-    mycursor.execute("UNLOCK TABLES;")
-    mycursor.close()
-    mydb.close()
-
-    if last_product is None or product_count is None:
-        return "No product found"
-
-    return f'{product_count[0]} products (last={last_product[0]})'
-
-
-def make_some_heavy_computation(param=""):
-    sleep(LONG_WAIT_TIME)
-    return f"Woody -{param}- Woody"
-
-
-def make_heavy_validation(order):
-    make_some_heavy_computation()
-    return "Success"
-
-
-def add_product(product):
-    mydb, mycursor = my_connect()
-    query = f"INSERT INTO woody.product ( name) VALUES ('{product}');"
-
-    mycursor.execute(query)
-    mydb.commit()
-    mycursor.close()
-    mydb.close()
-
-
-def launch_server(app, host='0.0.0.0', port=5000):
-    # voici ce qui rend le serveur si limité ...
-    run_simple(host, port, app, use_reloader=True, threaded=False)
-
-
-def save_order(order_id, status, product):
-    mydb, mycursor = my_connect()
-    query = f"INSERT INTO woody.order (order_id, status, product) VALUES ('{order_id}', '{status}', '{product}');"
-
-    mycursor.execute(query)
-    mydb.commit()
-
-    mycursor.close()
-    mydb.close()
-
-
 def get_order(order_id):
     mydb, mycursor = my_connect()
     query = f"SELECT status FROM woody.order WHERE order_id='{order_id}';"
@@ -99,3 +40,4 @@ def get_order(order_id):
 
     mycursor.close()
     mydb.close()
+    return order_status
